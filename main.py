85b8b6ffbba21e35c3c14e46755d912be4b944c7
Daves-devspace <126021050+Daves-devspace@users.noreply.github.com>
2025-05-01 14:09:36 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+import models, schemas, crud
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/students", response_model=list[schemas.Student])
+def read_students(db: Session = Depends(get_db)):
+    return crud.get_students(db)
+
+@app.post("/students", response_model=schemas.Student)
+def create_student(student: schemas.StudentCreate, db: Session = Depends(get_db)):
+    return crud.create_student(db, student)
