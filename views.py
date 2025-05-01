2ac77cd38f1ac7b60d8e11bccc342a5a3894c123
Daves-devspace <126021050+Daves-devspace@users.noreply.github.com>
2025-05-01 14:09:03 +0300

    Create views.py

---
+++ views.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+import models, schemas
+
+def get_students(db: Session):
+    return db.query(models.Student).all()
+
+def create_student(db: Session, student: schemas.StudentCreate):
+    db_student = models.Student(**student.dict())
+    db.add(db_student)
+    db.commit()
+    db.refresh(db_student)
+    return db_student
